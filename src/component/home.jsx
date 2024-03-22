ca2acb0de5c73cb43155e18460e2a9e74c3adaaa
BlessingLartey <bbklartey@gmail.com>
2024-03-22 17:53:18 +0000

    second trial

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,3 +1,5 @@
+import About from "./about";
+
 export default function Home() {
   return (
     <>
@@ -51,21 +53,274 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="hero-slider owl-carousel">
+        {/* <div className="hero-slider owl-carousel">
           <div
             className="hs-item set-bg"
-            data-setbg="img/hero/hero-1.jpg"
+            data-setbg="imghotel/aboutus.jpg"
           ></div>
           <div
             className="hs-item set-bg"
-            data-setbg="img/hero/hero-2.jpg"
+            data-setbg="imghotel/aboutus.jpg"
           ></div>
           <div
             className="hs-item set-bg"
-            data-setbg="img/hero/hero-3.jpg"
+            data-setbg="imghotel/aboutus.jpg"
           ></div>
+        </div> */}
+      </section>
+      <About />
+      {/* <!-- Home Room Section Begin --> */}
+      <section className="hp-room-section">
+        <div className="container-fluid">
+          <div className="hp-room-items">
+            <div className="row">
+              <div className="col-lg-3 col-md-6">
+                <div
+                  className="hp-room-item set-bg"
+                  data-setbg="img/room/room-b1.jpg"
+                >
+                  <div className="hr-text">
+                    <h3>Double Room</h3>
+                    <h2>
+                      199$<span>/Pernight</span>
+                    </h2>
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td className="r-o">Size:</td>
+                          <td>30 ft</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Capacity:</td>
+                          <td>Max persion 5</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Bed:</td>
+                          <td>King Beds</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Services:</td>
+                          <td>Wifi, Television, Bathroom,...</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <a href="#" className="primary-btn">
+                      More Details
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <div
+                  className="hp-room-item set-bg"
+                  data-setbg="img/room/room-b2.jpg"
+                >
+                  <div className="hr-text">
+                    <h3>Premium King Room</h3>
+                    <h2>
+                      159$<span>/Pernight</span>
+                    </h2>
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td className="r-o">Size:</td>
+                          <td>30 ft</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Capacity:</td>
+                          <td>Max persion 5</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Bed:</td>
+                          <td>King Beds</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Services:</td>
+                          <td>Wifi, Television, Bathroom,...</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <a href="#" className="primary-btn">
+                      More Details
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <div
+                  className="hp-room-item set-bg"
+                  data-setbg="img/room/room-b3.jpg"
+                >
+                  <div className="hr-text">
+                    <h3>Deluxe Room</h3>
+                    <h2>
+                      198$<span>/Pernight</span>
+                    </h2>
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td className="r-o">Size:</td>
+                          <td>30 ft</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Capacity:</td>
+                          <td>Max persion 5</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Bed:</td>
+                          <td>King Beds</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Services:</td>
+                          <td>Wifi, Television, Bathroom,...</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <a href="#" className="primary-btn">
+                      More Details
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-3 col-md-6">
+                <div
+                  className="hp-room-item set-bg"
+                  data-setbg="img/room/room-b4.jpg"
+                >
+                  <div className="hr-text">
+                    <h3>Family Room</h3>
+                    <h2>
+                      299$<span>/Pernight</span>
+                    </h2>
+                    <table>
+                      <tbody>
+                        <tr>
+                          <td className="r-o">Size:</td>
+                          <td>30 ft</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Capacity:</td>
+                          <td>Max persion 5</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Bed:</td>
+                          <td>King Beds</td>
+                        </tr>
+                        <tr>
+                          <td className="r-o">Services:</td>
+                          <td>Wifi, Television, Bathroom,...</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                    <a href="#" className="primary-btn">
+                      More Details
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
+      {/* <!-- Home Room Section End --> */}
+
+      <footer class="footer-section">
+        <div class="container">
+          <div class="footer-text">
+            <div class="row">
+              <div class="col-lg-4">
+                <div class="ft-about">
+                  <div class="logo">
+                    <a href="#">
+                      {/* <img src="img/footer-logo.png" alt=""> */}
+                    </a>
+                  </div>
+                  <p>
+                    We inspire and reach millions of travelers
+                    <br /> across 90 local websites
+                  </p>
+                  <div class="fa-social">
+                    <a href="#">
+                      <i class="fa fa-facebook"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fa fa-twitter"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fa fa-tripadvisor"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fa fa-instagram"></i>
+                    </a>
+                    <a href="#">
+                      <i class="fa fa-youtube-play"></i>
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-3 offset-lg-1">
+                <div class="ft-contact">
+                  <h6>Contact Us</h6>
+                  <ul>
+                    <li>(12) 345 67890</li>
+                    <li>info.colorlib@gmail.com</li>
+                    <li>856 Cordia Extension Apt. 356, Lake, United State</li>
+                  </ul>
+                </div>
+              </div>
+              <div class="col-lg-3 offset-lg-1">
+                <div class="ft-newslatter">
+                  <h6>New latest</h6>
+                  <p>Get the latest updates and offers.</p>
+                  <form action="#" class="fn-form">
+                    {/* <input type="text" placeholder="Email"> */}
+                    <button type="submit">
+                      <i class="fa fa-send"></i>
+                    </button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="copyright-option">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-7">
+                <ul>
+                  <li>
+                    <a href="#">Contact</a>
+                  </li>
+                  <li>
+                    <a href="#">Terms of use</a>
+                  </li>
+                  <li>
+                    <a href="#">Privacy</a>
+                  </li>
+                  <li>
+                    <a href="#">Environmental Policy</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="col-lg-5">
+                <div class="co-text">
+                  <p>
+                    {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
+                    Copyright &copy;
+                    <script>document.write(new Date().getFullYear());</script>{" "}
+                    All rights reserved | This template is made with{" "}
+                    <i class="fa fa-heart" aria-hidden="true"></i> by{" "}
+                    <a href="https://colorlib.com" target="_blank">
+                      Colorlib
+                    </a>
+                    {/* <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --> */}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
